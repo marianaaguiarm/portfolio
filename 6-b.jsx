f34eaf4cd0eddf701cc4edb1711e51a9165f36c0
marianaaguiarm <maarianaaguiarm@gmail.com>
2025-11-11 10:01:32 -0300

    6-b

---
+++ 6-b.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+
+function MostrarEsconder(){
+  const[mostrar, setMostrar] = useState(false);
+ 
+  return(
+  <div>
+    <button onClick={() => setMostrar (!mostrar)}>
+      {mostrar ? 'Esconder foto' : 'Mostrar foto'}
+    </button>
+    {mostrar && <img src="src/assets/foto.jpg"/>}
+  </div>
+
+
+  );
+}
+
+
+export default MostrarEsconder
